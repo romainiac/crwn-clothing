95ab152fbb162017317bdeaa294b74bdb24b39c4
Roman Yefimets <roman.yefimets1@t-mobile.com>
2021-10-27 13:11:05 -0700

    implemented firebase utils, included ability to store authenticated users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
     measurementId: "G-29BXQ475K2"
   };
 
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+
+    console.log(snapShot)
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
