a83b6278149a7951f353d07c7c0e69efad0f1888
Roman Yefimets <roman.yefimets1@t-mobile.com>
2021-10-28 12:29:35 -0700

    added checkout page, checkout item component and updated cart to use selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,9 @@ import {connect} from 'react-redux'
 import CartIcon from '../cart-icon/cart-icon.component';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import {createStructuredSelector} from 'reselect'
+import {selectCartHidden} from '../../redux/cart/cart.selectors'
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import {auth} from '../../firebase/firebase.utils'
 
@@ -38,9 +41,9 @@ const Header = ({currentUser, hidden}) => (
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser, 
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,13 +2,33 @@ import {createSelector} from 'reselect'
 
 const selectCart = state => state.cart;
 
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+)
+
 export const selectCartItems = createSelector(
     [selectCart],
-    (cart) => cart.cartItems
+    cart => cart.cartItems
 )
 
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => 
-        cartItems.reduce((accumulatedQuanitity, cartItem) => accumulatedQuanitity + cartItem.quantity, 0) 
+        cartItems.reduce(
+            (accumulatedQuanitity, cartItem) => 
+                accumulatedQuanitity + cartItem.quantity, 
+            0
+            ) 
+)
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => 
+        cartItems.reduce(
+            (accumulatedQuanitity, cartItem) => 
+                accumulatedQuanitity + cartItem.quantity * cartItem.price, 
+            0
+            ) 
+
 )
