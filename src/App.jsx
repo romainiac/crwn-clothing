fc77a820027d2d72657f1fff72ee815a3df9bace
Roman Yefimets <roman.yefimets1@t-mobile.com>
2021-11-30 09:40:35 -0800

    added redux hooks for collection and cart dropdown component

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import './App.css';
 
 import React, {useEffect} from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom'
-import {connect} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
-import {selectCurrentUser} from './redux/user/user.selectors'
-import {createStructuredSelector} from 'reselect'
-import {checkUserSession} from './redux/user/user.actions'
 
-const App = ({checkUserSession, currentUser}) => {
+import {selectCurrentUser} from './redux/user/user.selectors'
+import { checkUserSession } from './redux/user/user.actions';
+
+const App = () => {
+
+  const currentUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
 
   useEffect(()=> {
-    checkUserSession()
-  }, [checkUserSession])
+    dispatch(checkUserSession())
+  }, [dispatch])
 
   return (
     <div>
@@ -32,13 +35,4 @@ const App = ({checkUserSession, currentUser}) => {
   );
 }
 
-const mapStateToProps = createStructuredSelector({
-  currentUser : selectCurrentUser
-})
-
-const mapDispatchToProps = dispatch => ({
-  checkUserSession: () => dispatch(checkUserSession())
-})
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
